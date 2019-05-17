3643c87e3a70ce10faa7b820d1469eb2d5a01442
James Pier <jpier2012@gmail.com>
2019-05-17 21:27:34 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,7 +28,7 @@ class InteractiveRecord
     column_names.compact
   end
 
-  def initialize(options = {}, id = nil)
+  def initialize(options = {})
     options.each do |key, value|
       self.send("#{key}=", value)
     end
