ffc02f74c979f9e350f2a492b85d9b45f552ebda
James Pier <jpier2012@gmail.com>
2019-05-17 21:09:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,14 +3,5 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  def self.table_name
-    self.to_s.downcase.pluralize
-  end
-
-  def self.column_names
-    DB[:conn].results_has_hash = true
-
-    sql = "PRAGMA table_info('#{table_name}')"
-  end
 
 end
