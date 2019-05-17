22cdd0a05905da847f1bd2a151a7c66421bb560d
James Pier <jpier2012@gmail.com>
2019-05-17 20:56:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,4 +7,10 @@ class Student < InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_has_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+  end
+
 end
