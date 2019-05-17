732e34bee660d1b936d6cf8022951638918da8aa
James Pier <jpier2012@gmail.com>
2019-05-17 20:56:54 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,16 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_has_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    
+  end
+
 end
