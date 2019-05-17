0d37ee9e33db1279482468ff593b799581f05dad
James Pier <jpier2012@gmail.com>
2019-05-17 22:22:22 +0000

    Automatically backed up by Learn

--- scripts.sql
+++ scripts.sql
@@ -1,12 +1,6 @@
-DB = {:conn => SQLite3::Database.new("db/students.db")}
+CREATE TABLE IF NOT EXISTS students (
+id INTEGER PRIMARY KEY,
+name TEXT,
+grade INTEGER
+)
 
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS students (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  grade INTEGER
-  )
-SQL
-
-DB[:conn].execute(sql)
-DB[:conn].results_as_hash = true
