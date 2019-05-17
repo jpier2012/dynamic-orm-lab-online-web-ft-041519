d0eaeefbb1a2eceaefe109de12fb252563dd5655
James Pier <jpier2012@gmail.com>
2019-05-17 22:27:38 +0000

    Automatically backed up by Learn

--- scripts.sql
+++ scripts.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS students (
 id INTEGER PRIMARY KEY,
 name TEXT,
 grade INTEGER
-)
+);
 
 INSERT INTO students (id, name, grade)
-VALUES (1, 'Susan', 10)
+VALUES (1, 'Susan', 10);
