d353ea7bcb781ab05ff5b5ddb61ce22a4b72bfec
James Pier <jpier2012@gmail.com>
2019-05-17 22:01:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,7 +60,7 @@ class InteractiveRecord
     INSERT INTO ? (?)
     VALUES (?)
     SQL
-    
+
     DB[:conn].execute(sql, table_name_for_insert, col_names_for_insert, values_for_insert)
     @id = DB[:conn].execute('SELECT last_insert_rowid() FROM ? ', table_name_for_insert)[0][0]
   end
